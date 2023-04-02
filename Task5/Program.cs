f97201bde2d6e9b0ded006ef745e76dbd37271b9
Денис Пономарев <deskin196@gmail.com>
2023-04-02 20:59:33 +0300

    Написал алгоритм по ДЗ 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -6,3 +6,63 @@
 11 16 15 06
 10 09 08 07
 */
+
+const int ROW = 0;
+const int COLUMN = 1;
+
+
+//Выводит двумерный массив на экран
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Функция заполняет по спирали двумерный массив любой размерности
+void FillSpiralMatrix(int[,] matrix)
+{
+    int dRow = 0; //Приращение по строкам
+    int dColumn = 1; //Приращение по столбцам
+    int row = 0;
+    int column = 0;
+
+    for (int i = 1; i <= matrix.GetLength(ROW) * matrix.GetLength(COLUMN); i++)
+    {
+        matrix[row, column] = i;
+        
+        
+        //Если следующий шаг уже заполнен (не 0) или выйдем за пределы индекса (стоим на краю), то поворачиваем по часовой стрелке
+        if (
+            row + dRow >= matrix.GetLength(ROW) ||
+            column + dColumn >= matrix.GetLength(COLUMN)||
+            row + dRow < 0 ||
+            column + dColumn < 0||
+            matrix[row + dRow, column + dColumn] != 0
+        )
+        {
+            //Если двигались направо, то двигаемся вниз
+            if (dRow == 0 && dColumn == 1) { dRow = 1; dColumn = 0; }
+            //Если двигались вниз, то двигаемся налево
+            else if (dRow == 1 && dColumn == 0) { dRow = 0; dColumn = -1; }
+            //Если двигались налево, то двигаемся вверх
+            else if (dRow == 0 && dColumn == -1) { dRow = -1; dColumn = 0; }
+            //Если двигались вверх, то двигаемся направо
+            else { dRow = 0; dColumn = 1; }
+        }
+        row += dRow;
+        column += dColumn;
+    }
+}
+
+
+int[,] matrix = new int[4, 4];
+
+FillSpiralMatrix(matrix);
+PrintMatrix(matrix);
